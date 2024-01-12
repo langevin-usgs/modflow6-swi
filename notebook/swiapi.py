7869dc63826fb2805296cd810a6615d50813f071
Langevin, Christian D <langevin@usgs.gov>
2024-01-12 17:14:01 -0600

    Attempt to get fresh and salt models working in api; not working yet

--- notebook/swiapi.py
+++ notebook/swiapi.py
@@ -2,13 +2,43 @@ import numpy as np
 import modflowapi
 
 
+def get_model_variables(modelname):
+    model_variables = [
+    # gwf
+    ("insto", modelname),
+    ("iss", modelname),
+    ("x", modelname),
+    ("xold", modelname),
+    ("rhs", modelname),
+
+    # dis
+    ("area", modelname, "dis"),
+    ("top", modelname, "dis"),
+    ("bot", modelname, "dis"),
+
+    # npf
+    ("sat", modelname, "npf"),
+    ("condsat", modelname, "npf"),
+    ("derv_mult", modelname, "npf"),
+
+    # swi
+    ("zeta", modelname, "swi"),
+    ("hcof", modelname, "swi"),
+    ("rhs", modelname, "swi"),
+    ]
+    return model_variables
+
+
 class SwiAPI:
 
-    def __init__(self, libmf6, ws, modelname):
+    def __init__(self, libmf6, ws, modelname, salt_modelname=None):
         self.mf6api =  modflowapi.ModflowApi(libmf6, working_directory=ws)
         self.modelname = modelname
+        self.salt_modelname = salt_modelname
         self.initialize()
-        self.api_pointer = {}
+        self.sim_pointer = {}
+        self.model_fresh_pointer = {}
+        self.model_salt_pointer = {}
         self.zeta_all = []
         return
     
@@ -22,86 +52,100 @@ class SwiAPI:
     
     def create_pointers(self, verbose):
         """Create pointers to modflow variables and store in
-           self.api_pointer.
+           self.sim_pointer, self.model_fresh_pointer, 
+           self.model_salt_pointer.
         """
-        api_pointers = [
+        sim_variables = [
 
             # tdis
             ("delt", 'tdis'),
             ("nper", 'tdis'),
 
-            # gwf
-            ("insto", self.modelname),
-            ("iss", self.modelname),
-            ("x", self.modelname),
-            ("xold", self.modelname),
-            ("rhs", self.modelname),
-
             # sln
             ("ia", "sln_1"),
             ("ja", "sln_1"),
             ("amat", "sln_1"),
-
-            # dis
-            ("area", self.modelname, "dis"),
-            ("top", self.modelname, "dis"),
-            ("bot", self.modelname, "dis"),
-
-            # npf
-            ("sat", self.modelname, "npf"),
-            ("condsat", self.modelname, "npf"),
-            ("derv_mult", self.modelname, "npf"),
-
-            # swi
-            ("zeta", self.modelname, "swi"),
-            ("hcof", self.modelname, "swi"),
-            ("rhs", self.modelname, "swi"),
         ]
 
-        # add list of pointers to self.api_pointer dict
-        for api_pointer in api_pointers:
-            self.add_pointer(api_pointer, verbose)
+        # add list of simulation pointers to self.sim_pointer dict
+        for v in sim_variables:
+            self.add_pointer(self.sim_pointer, v, verbose)
+
+        # add list of pointers to fresh model dictionary
+        for v in get_model_variables(self.modelname):
+            self.add_pointer(self.model_fresh_pointer, v, verbose)
+
+        # add list of pointers to salt model dictionary
+        if self.salt_modelname is not None:
+            for v in get_model_variables(self.salt_modelname):
+                self.add_pointer(self.model_salt_pointer, v, verbose)
 
         # check for storage and add storage pointers
-        if self.api_pointer["insto"] > 0:
-            api_pointer = ("sy", self.modelname, "sto")
-            self.add_pointer(api_pointer, verbose)
+        if self.model_fresh_pointer["insto"] > 0:
+            v = ("sy", self.modelname, "sto")
+            self.add_pointer(self.model_fresh_pointer, v, verbose)
 
-        # set derivative multiplier
-        derv_mult = self.api_pointer["derv_mult"]
+        # add list of pointers to salt model dictionary
+        if self.salt_modelname is not None:
+            if self.model_salt_pointer["insto"] > 0:
+                v = ("sy", self.salt_modelname, "sto")
+                self.add_pointer(self.model_salt_pointer, v, verbose)
+
+        # set derivative multiplier for freshwater
+        derv_mult = self.model_fresh_pointer["derv_mult"]
         derv_mult[:] = self.alpha_f + 1.0
 
-    def add_pointer(self, api_pointer, verbose):
-        variable_name = api_pointer[0]
-        args = [item.upper() for item in api_pointer]
+        # set derivative multiplier for saltwater
+        if self.salt_modelname is not None:
+            derv_mult = self.model_salt_pointer["derv_mult"]
+            derv_mult[:] = self.alpha_s
+
+    def add_pointer(self, pointer_dict, variable, verbose):
+        variable_name = variable[0]
+        args = [item.upper() for item in variable]
         tag = self.mf6api.get_var_address(*args)
         if verbose:
             print(f"Accessing pointer using tag: {tag}")
         pointer = self.mf6api.get_value_ptr(tag)
-        self.api_pointer[variable_name] = pointer
+        pointer_dict[variable_name] = pointer
 
     def print_pointers(self):
-        """Print pointers stored in self.api_pointer"""
-        for variable_name in self.api_pointer:
-            pointer = self.api_pointer[variable_name]
-            print(f"{variable_name}: {pointer}")
+        """Print pointers to MODFLOW variables"""
+        for pointer_dict in [
+            self.sim_pointer, 
+            self.model_fresh_pointer, 
+            self.model_salt_pointer]:
+            for variable_name in pointer_dict:
+                pointer = pointer_dict[variable_name]
+                print(f"{variable_name}: {pointer}")
 
     def update_zeta(self):
-        """Recalculate zeta using the pointer to the modflow head"""
-        zeta = self.api_pointer["zeta"]
-        self.zeta_last = zeta.copy()
-        head_freshwater = self.api_pointer["x"]
-        zeta[:] = (
-            self.alpha_s * self.head_saltwater - self.alpha_f * head_freshwater
-        )
+        """Recalculate zeta using the pointer to the modflow 
+        freshwater head and saltwater head (if saltwater model
+        is present)."""
+        zeta_fresh = self.model_fresh_pointer["zeta"]
+        self.zeta_last = zeta_fresh.copy()
+        head_freshwater = self.model_fresh_pointer["x"]
+        head_saltwater = self.head_saltwater
+        if self.salt_modelname is not None:
+            head_saltwater = self.model_salt_pointer["x"]
+            zeta_salt = self.model_salt_pointer["zeta"]
+        zeta = self.alpha_s * head_saltwater - self.alpha_f * head_freshwater
+        zeta_fresh[:] = zeta
+        if self.salt_modelname is not None:
+            zeta_salt[:] = zeta
+        print (f"Setting {zeta=}")
 
     def formulate(self, dt):
-        is_transient = self.api_pointer["iss"] == 0
+        is_transient = self.model_fresh_pointer["iss"] == 0
         if is_transient:
-            self.formulate_storage(dt)
+            self.formulate_storage(dt, self.model_fresh_pointer, self.alpha_f)
+            if self.salt_modelname is not None:
+                # flip sign for saltwater model
+                self.formulate_storage(dt, self.model_salt_pointer, -self.alpha_s)
         return
 
-    def formulate_storage(self, dt):
+    def formulate_storage(self, dt, model_pointer, alpha):
         """
         Calculate hcof and rhs in the modflow swi package to 
         account for the change in freshwater storage.
@@ -109,20 +153,22 @@ class SwiAPI:
             hcof = - alpha_f * area * S_zeta / delt
             rhs = - alpha_f * area * S_zeta / delt * h_old 
 
+        This function is intended to work for either the freshwater
+        model or the saltwater model (if present)
         """
-        head = self.api_pointer["x"]
-        hold = self.api_pointer["xold"]
-        hcof = self.api_pointer["hcof"]
-        rhs = self.api_pointer["rhs"]
-        zeta = self.api_pointer["zeta"]
-        top = self.api_pointer["top"]
-        bot = self.api_pointer["bot"]
-        area = self.api_pointer["area"]
-        sy = self.api_pointer["sy"]
+        head = model_pointer["x"]
+        hold = model_pointer["xold"]
+        hcof = model_pointer["hcof"]
+        rhs = model_pointer["rhs"]
+        zeta = model_pointer["zeta"]
+        top = model_pointer["top"]
+        bot = model_pointer["bot"]
+        area = model_pointer["area"]
+        sy = model_pointer["sy"]
         s_zeta = np.zeros(head.shape)
         idx = (zeta > bot) & (zeta <= top)
         s_zeta[idx] = sy[idx]
-        sc = - self.alpha_f / dt * np.multiply(area, s_zeta)
+        sc = - alpha / dt * np.multiply(area, s_zeta)
         hcof[:] = sc
         rhs[:] = sc * hold
 
@@ -147,7 +193,7 @@ class SwiAPI:
             if verbose:
                 print(f"\n  Solving for time {current_time}")
             # dt = self.mf6api.get_time_step()
-            dt = self.api_pointer["delt"]
+            dt = self.sim_pointer["delt"]
 
             if verbose:
                 print(f"  Prepare time step with dt={dt}...")
